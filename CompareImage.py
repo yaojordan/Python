977744793d1ca432557336534432c31d89c13027
宇鴻 <yaojordan@gmail.com>
2017-09-24 15:00:01 +0800

    Update CompareImage.py

--- CompareImage.py
+++ CompareImage.py
@@ -1,61 +1,7 @@
 #There is MSE and SSIM, two different ways to compare images
 #MSE compare entire images, when value equals 0, means perfectly similarity
 #SSIM indicate structual similarity
-
-from skimage.measure import structual_similarity as ssim
-import matplotlib.pyplot as plt
-import numpy as np
-import cv2 #for OpenCV binding
-
-def mse(imageA, imageB):
-  # the 'Mean Squared Error' between the two images is the
-	# sum of the squared difference between the two images;
-	# NOTE: the two images must have the same dimension
-  err = np.sum((imageA.astype("float") - imageB.astype("float")) ** 2)
-  err /= float(imageA.shape[0] * imageA.shape[1])
-	
-	# return the MSE, the lower the error, the more "similar"
-	# the two images are
-	return err
   
-def compare_images(imageA, imageB, title):
-  # compute the mean squared error and structural similarity
-	# index for the images
-  m = mse(imageA, imageB)
-  s = ssim(imageA, imageB)
-  
-  
-  # setup the figure
-	fig = plt.figure(title)
-	plt.suptitle("MSE: %.2f, SSIM: %.2f" % (m, s))
-  
-  # show first image
-	ax = fig.add_subplot(1, 2, 1)
-	plt.imshow(imageA, cmap = plt.cm.gray)
-	plt.axis("off")
- 
-	# show the second image
-	ax = fig.add_subplot(1, 2, 2)
-	plt.imshow(imageB, cmap = plt.cm.gray)
-	plt.axis("off")
- 
-	# show the images
-	plt.show()
-  
-  
-  # load the images -- the original, the original + contrast,
-  # and the original + photoshop
-  original = cv2.imread("images/jp_gates_original.png")
-  contrast = cv2.imread("images/jp_gates_contrast.png")
-  shopped = cv2.imread("images/jp_gates_photoshopped.png")
- 
-  # convert the images to grayscale
-  original = cv2.cvtColor(original, cv2.COLOR_BGR2GRAY)
-  contrast = cv2.cvtColor(contrast, cv2.COLOR_BGR2GRAY)
-  shopped = cv2.cvtColor(shopped, cv2.COLOR_BGR2GRAY)
-  
-  hon Compare Two ImagesPython
-
 # import the necessary packages
 from skimage.measure import structural_similarity as ssim
 import matplotlib.pyplot as plt
